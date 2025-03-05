d041d45427347ec4a851aacd7da60a9ce47ab8af
akhilesh.g <akhilesh.g@qburst.com>
2025-03-05 10:28:21 +0530

    Started practicing 8weeksqlchallenge.com, added documentation.

---
+++ NOTES.md
@@ -0,0 +1,10 @@
+***Batch Deletion***
+
+10000 rows per commit, 1 second delay
+Table locks for each 10000 rows until transaction commits.
+
+dead lock
+
+Index updation for each commit
+
+WAL for each commit, replication lags.
---
+++ dannys_diner.sql
@@ -0,0 +1,47 @@
+-- DDL
+
+BEGIN;
+CREATE SCHEMA dannys_diner;
+SET search_path = dannys_diner;
+
+CREATE TABLE members(customer_id VARCHAR(1) PRIMARY KEY, join_date DATE NOT NULL);
+
+CREATE TABLE menu(product_id INT, product_name VARCHAR(5) NOT NULL, price INT NOT NULL, PRIMARY KEY (product_id));
+
+CREATE TABLE sales(customer_id VARCHAR(1) REFERENCES members(customer_id), order_date DATE NOT NULL, product_id INT REFERENCES menu(product_id));
+
+-- DML
+
+INSERT INTO menu
+  ("product_id", "product_name", "price")
+VALUES
+  ('1', 'sushi', '10'),
+  ('2', 'curry', '15'),
+  ('3', 'ramen', '12');
+  
+INSERT INTO members
+  ("customer_id", "join_date")
+VALUES
+  ('A', '2021-01-07'),
+  ('B', '2021-01-09'),
+  ('C', '2021-01-10');
+
+INSERT INTO sales
+  ("customer_id", "order_date", "product_id")
+VALUES
+  ('A', '2021-01-01', '1'),
+  ('A', '2021-01-01', '2'),
+  ('A', '2021-01-07', '2'),
+  ('A', '2021-01-10', '3'),
+  ('A', '2021-01-11', '3'),
+  ('A', '2021-01-11', '3'),
+  ('B', '2021-01-01', '2'),
+  ('B', '2021-01-02', '2'),
+  ('B', '2021-01-04', '1'),
+  ('B', '2021-01-11', '1'),
+  ('B', '2021-01-16', '3'),
+  ('B', '2021-02-01', '3'),
+  ('C', '2021-01-01', '3'),
+  ('C', '2021-01-01', '3'),
+  ('C', '2021-01-07', '3');
+COMMIT;
